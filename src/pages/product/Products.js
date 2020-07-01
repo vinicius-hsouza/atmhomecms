a3b088a8ed3ae4e4e7656f1ed815054aeeb0333a
Vinicius Henrique de Souza <vinicius@hvsti.com.br>
2020-07-01 20:16:13 -0300

    correcao em produtos

--- src/pages/product/Products.js
+++ src/pages/product/Products.js
@@ -1,40 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import {
-  Row,  Button, Typography, message,
-} from 'antd';
+import { Row, Button, Typography, message } from "antd";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import api from '../../services/api';
+import api from "../../services/api";
 
-import { Image } from './styles';
+import { Image } from "./styles";
 
-import './styles.css';
+import "./styles.css";
 
 export default function Products({ history }) {
   const [data, setData] = useState([]);
 
-  const noImages = [
-    { key: 1 },
-    { key: 2 },
-    { key: 3 },
-  ];
+  const noImages = [{ key: 1 }, { key: 2 }, { key: 3 }];
 
   const { Title } = Typography;
 
   async function allProducts() {
-    const response = await api.get('/product');
+    const response = await api.get("/product");
     setData(response.data);
   }
 
   async function deleteProduct(id, name) {
     const response = await api.delete(`/product/${id}`);
 
-    await setData(data.filter(item => item.id !== id));
+    await setData(data.filter((item) => item.id !== id));
 
     if (response.status === 204) {
-      message.success('O Produto foi excluido com sucesso!');
+      message.success("O Produto foi excluido com sucesso!");
     }
   }
 
@@ -47,36 +41,44 @@ export default function Products({ history }) {
       <div className="list-item">
         <div>
           <Link to={`/product/edit/${product.id}`}>Editar</Link>
-          <a href="#" onClick={() => deleteProduct(product.id)}>Excluir</a>
+          <a href="#" onClick={() => deleteProduct(product.id)}>
+            Excluir
+          </a>
         </div>
         <p>{product.name}</p>
 
-        {product.images.length ? product.images.map(image => <Image key={image.id} image={image.url} />)
-          : noImages.map(image => <Image key={image.key} image="http://www.embaleme.com.br/img/noimage.jpg" />)}
-
-
+        {product.images && product.images.length
+          ? product.images.map((image) => (
+              <Image key={image.id} image={image.url} />
+            ))
+          : noImages.map((image) => (
+              <Image
+                key={image.key}
+                image="http://www.embaleme.com.br/img/noimage.jpg"
+              />
+            ))}
       </div>
     );
   }
 
   return (
     <div>
-        <Row type="flex" justify="space-between" style={{ marginBottom: '20px' }}>
-          <Title level={4}>
-            Produtos
-          </Title>
-          <Button
-            type="primary"
-            shape="round"
-            icon="plus"
-            className="button-title-action"
-            onClick={() => history.push('/product/new')}
-          >
-            Adicionar
-          </Button>
-        </Row>
+      <Row type="flex" justify="space-between" style={{ marginBottom: "20px" }}>
+        <Title level={4}>Produtos</Title>
+        <Button
+          type="primary"
+          shape="round"
+          icon="plus"
+          className="button-title-action"
+          onClick={() => history.push("/product/new")}
+        >
+          Adicionar
+        </Button>
+      </Row>
 
-      {data.map(product => <ListItem key={product.id} product={product} />)}
+      {data.map((product) => (
+        <ListItem key={product.id} product={product} />
+      ))}
     </div>
   );
 }
